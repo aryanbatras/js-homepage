2408d025632a764300ab067752c9366002ad3b84
aryanbatras <batraaryan03@gmail.com>
2026-01-23 05:45:03 +0530

    deploy2

--- src/pages/homepage/section-macbook/index.jsx
+++ src/pages/homepage/section-macbook/index.jsx
@@ -140,8 +140,8 @@ function MacModel({
 }) {
   const gltfPath =
     process.env.NODE_ENV === "production"
-      ? "/js-hub/mac-draco.glb"
-      : "/mac-draco.glb";
+      ? "/js-homepage/mac-draco.glb"
+      : "/js-homepage/mac-draco.glb";
   const { nodes, materials } = useGLTF(gltfPath);
   useEffect(() => {
     setReady(true);
